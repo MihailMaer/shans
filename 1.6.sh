28b6c3015103da1bd64be7a7e3ff604d6289f3e7
Mihail <mihon261206@gmail.com>
2026-06-15 16:25:59 +0300

    update

--- 1.6.sh
+++
@@ -1,8 +0,0 @@
-# Включаем forwarding в sysctl
-if grep -q "^net.ipv4.ip_forward" /etc/net/sysctl.conf; then
-    sed -i 's/^net.ipv4.ip_forward.*/net.ipv4.ip_forward = 1/' /etc/net/sysctl.conf
-else
-    echo "net.ipv4.ip_forward = 1" >> /etc/net/sysctl.conf
-fi
-sysctl -w net.ipv4.ip_forward=1
-echo "Yes"
--- 1.7.sh
+++
@@ -1,3 +0,0 @@
-apt-get update
-apt-get install -y iptables
-echo "Yes 1.7"
